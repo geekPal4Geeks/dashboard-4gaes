69b7d7c2236affd2de929f90b4349a087e0bb7d8
Ehiber Graterol <ehiber5@hotmail.com>
2025-05-27 08:08:15 -0400

    ➕ Add routes for Curses, Mentorships, and Courses Management modules

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,6 +5,7 @@ import {
     Route,
 } from "react-router-dom"
 import { CircularProgress, Box } from '@mui/material'
+import ProtectedRoute from './components/ProtectedRoute'
 
 // Pages
 const Layout = lazy(() => import('./pages/Layout'))
@@ -12,6 +13,9 @@ const Home = lazy(() => import('./pages/Home'))
 const NotFound = lazy(() => import('./pages/NotFound'))
 const Login = lazy(() => import('./pages/Login'))
 const GithubCallback = lazy(() => import('./pages/GithubCallback'))
+const Curses = lazy(() => import('./pages/Curses'))
+const Mentorships = lazy(() => import('./pages/Mentorships'))
+const CoursesManagement = lazy(() => import('./pages/CoursesManagement'))
 
 // Loading component
 const LoadingFallback = () => (
@@ -41,6 +45,13 @@ export const router = createBrowserRouter(
             {/* Resto de la app con layout */}
             <Route element={<LazyRoute Component={Layout} />}>
                 <Route path="/home" element={<LazyRoute Component={Home} />} />
+                <Route path="/curses" element={<LazyRoute Component={Curses} />} />
+                <Route path="/mentorships" element={<LazyRoute Component={Mentorships} />} />
+                <Route path="/courses-management" element={
+                  <ProtectedRoute allowedRoles={['academy_coordinator', 'country_manager']}>
+                    <LazyRoute Component={CoursesManagement} />
+                  </ProtectedRoute>
+                } />
                 <Route path="*" element={<LazyRoute Component={NotFound} />} />
             </Route>
         </>
